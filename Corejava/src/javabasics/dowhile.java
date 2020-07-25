0698e88e21c0efccf40870373637520e2cea4fc0
Samapiika <samapika.gantayat@tavant.com>
2020-07-25 22:47:57 +0530

    add changes

---
+++ Corejava/src/javabasics/dowhile.java
@@ -0,0 +1,18 @@
+package javabasics;
+
+public class dowhile {
+int i=0;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		do
+		{
+			System.out.println("counter:"+i);
+			i++;
+		}
+		
+		while(i<=10);
+
+	}
+
+}
