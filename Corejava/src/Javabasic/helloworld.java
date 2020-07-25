b584f00f5520707287a5d3a44da4db9c3786d064
Samapiika <samapika.gantayat@tavant.com>
2020-07-25 20:56:15 +0530

    test changes

---
+++ Corejava/src/Javabasic/helloworld.java
@@ -0,0 +1,14 @@
+package Javabasic;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s1="hello word";
+		System.out.println(s1);
+		
+
+	}
+
+}
